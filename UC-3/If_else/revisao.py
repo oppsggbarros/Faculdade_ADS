89b06177cba848ac4028dd48034afdc88d4d970f
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2025-03-31 21:50:49 -0400

    Par e impar python

---
+++ UC-3/If_else/revisao.py
@@ -0,0 +1,6 @@
+dia = input("Digite o dia da semana: ").upper()
+if dia == "QUARTA":
+    print("Assistir jogo do Timão!!")
+else: 
+    print("Estudar")
+    
---
+++ UC-3/Python/parouimpar.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero: "))
+if numero % 2 == 0:
+    print("é par")
+else:
+    print("é ímpar")
